63e8244578e53696ee92918004467c7d17294769
Agustin Settimo <agustinsettimo.ips@gmail.com>
2021-02-21 23:33:51 -0300

    Add pcf_print in c code generator

--- src/GCC.hs
+++ src/GCC.hs
@@ -1,6 +1,6 @@
 module GCC ( ppC ) where
 import Data.Text.Prettyprint.Doc
-import Data.Text ( unpack )
+import Data.Text ( Text, unpack )
 import Data.Text.Prettyprint.Doc.Render.Terminal ( renderStrict )
 import Closure
 import Lang (BinaryOp(Minus, Plus),  Const(CNat))
@@ -12,12 +12,14 @@ decl2doc (IrFun n args t) =
   braces (nest 2 (line <> pretty "return" <+> ir2doc t <> semi) <> line)
 
 pcfMain :: [IrDecl] -> Doc a
-pcfMain xs = pretty "uint64_t* pcfmain()" <+> braces (nest 2 (line <> vsep (pcfMain' xs ++ [pretty "return 0;"])) <> line)
+pcfMain xs = pretty "uint64_t* pcfmain()" <+> braces (nest 2 (line <> vsep (pcfMain' vs ++ [pretty "return 0;"])) <> line)
+  where vals (IrVal _ _) = True
+        vals _           = False
+        vs = filter vals xs
 
 pcfMain' :: [IrDecl] -> [Doc a]
-pcfMain' [] = []
+pcfMain' [IrVal n t] = [pretty n <+> pretty "=" <+> ir2doc t <> semi, irPrint n]
 pcfMain' (IrVal n t : xs) = (pretty n <+> pretty "=" <+> ir2doc t <> semi) : pcfMain' xs
-pcfMain' (_ : xs) = pcfMain' xs
 
 stmt :: Doc a -> Doc a
 stmt x = parens (braces (nest 2 (line <> x <> semi) <> line))
@@ -28,7 +30,7 @@ ir2doc (IrCall f args) = parens (pretty "(void* (*) (void*, void*))" <+> ir2doc 
 ir2doc (IrConst (CNat n)) = pretty n
 ir2doc (IrBinaryOp op a b) = pretty "(int)" <+> ir2doc a <+> op2doc op <+> pretty "(int)" <+> ir2doc b
 ir2doc (IrLet n t t') = stmt $ hsep [pretty "void**", pretty n, pretty "=",  ir2doc t] <> semi <> line <> ir2doc t'
-ir2doc (IrIfZ c a b) = sep [ir2doc c, nest 2 (pretty "?" <+> ir2doc a), nest 2 (colon <+> ir2doc b)]
+ir2doc (IrIfZ c a b) = sep [ir2doc c, nest 2 (pretty "?" <+> ir2doc b), nest 2 (colon <+> ir2doc a)]
 ir2doc (MkClosure f args) = pretty "pcf_mkclosure" <> tupled (pretty f : pretty (length args) : map ir2doc args)
 ir2doc (IrAccess t i) = parens (ir2doc t) <> brackets (pretty i)
 
@@ -37,7 +39,10 @@ op2doc Plus  = pretty "+"
 op2doc Minus = pretty "-"
 
 prelude :: Doc a
-prelude = pretty "extern void *pcf_mkclosure(void*, int, ...);"
+prelude = pretty "#include <inttypes.h>" <> line <> pretty "extern void *pcf_mkclosure(void*, int, ...);"
 
-ppC :: [IrDecl] -> String 
+irPrint :: String -> Doc a
+irPrint x = pretty "pcf_print" <> tupled [pretty x] <> semi
+
+ppC :: [IrDecl] -> String
 ppC xs = unpack . renderStrict . layoutSmart defaultLayoutOptions $ vsep (prelude : map decl2doc xs ++ [pcfMain xs])
